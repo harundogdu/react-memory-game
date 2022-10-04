f4cdbfc64a2d0a94f82a12f64a04ddecb1dc1cce
Harun Doğdu <Harundogdu06@gmail.com>
2022-10-04 17:59:23 +0300

    feat(memory-game) : memory Card Array created

---
+++ src/utils/fake_db.ts
@@ -0,0 +1,33 @@
+export const CardArray = [
+  {
+    image: '/img/helmet.png',
+    isFlipped: false,
+    isMatched: false,
+    name: 'Helmet'
+  },
+  {
+    image: '/img/potion.png',
+    isFlipped: false,
+    isMatched: false,
+    name: 'Potion'
+  },
+  {
+    image: '/img/scroll.png',
+    isFlipped: false,
+    isMatched: false,
+    name: 'Scroll'
+  },
+  {
+    image: '/img/sword.png',
+    isFlipped: false,
+    isMatched: false,
+    name: 'Sword'
+  },
+  { image: '/img/ring.png', isFlipped: false, isMatched: false, name: 'Ring' },
+  {
+    image: '/img/shield.png',
+    isFlipped: false,
+    isMatched: false,
+    name: 'Shield'
+  }
+];
