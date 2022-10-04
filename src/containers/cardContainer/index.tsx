4d1883225a747bc4f3ba450a660044ae7c07815f
Harun Doğdu <Harundogdu06@gmail.com>
2022-10-04 19:25:28 +0300

    feat(memory-game) : sweet alert used for winners

--- src/containers/cardContainer/index.tsx
+++ src/containers/cardContainer/index.tsx
@@ -1,8 +1,30 @@
 import { Card } from '../../components';
 import { useMemoryCards } from '../../contexts/memoryContext';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 const CardContainer = () => {
-  const { cards, handleCardItemClick, disabledCards } = useMemoryCards();
+  const { cards, handleCardItemClick, disabledCards, checkWin, turn, startGame } =
+    useMemoryCards();
+
+  if (checkWin()) {
+    const MySwal = withReactContent(Swal);
+    MySwal.fire({
+      background: '#000',
+      color: '#fff',
+      title: "You've won!",
+      text: `You took ${turn} turns to complete the game!`,
+      icon: 'success',
+      confirmButtonText: 'Play Again',
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      allowEnterKey: false,
+      preConfirm: () => {
+        startGame();
+      }
+    });
+  }
+
   return (
     <div className='card-container'>
       {cards.map((card, index) => (
