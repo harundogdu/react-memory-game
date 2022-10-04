31499303830cd14ea8ee759f16b1603ae8e3a762
Harun Doğdu <Harundogdu06@gmail.com>
2022-10-04 18:52:59 +0300

    feat(memory-game) : flip and disable card function created

--- src/contexts/memoryContext.tsx
+++ src/contexts/memoryContext.tsx
@@ -12,6 +12,7 @@ type MemoryContextType = {
   startGame: () => void;
   turn: number;
   handleCardItemClick: (card: IMemoryCard) => void;
+  disabledCards: boolean;
 };
 
 const initialState = {
@@ -19,7 +20,8 @@ const initialState = {
   setCards: () => {},
   startGame: () => {},
   turn: 0,
-  handleCardItemClick: () => {}
+  handleCardItemClick: () => {},
+  disabledCards: false
 };
 
 const MemoryContext = createContext<MemoryContextType>(initialState);
@@ -27,7 +29,16 @@ const MemoryContext = createContext<MemoryContextType>(initialState);
 const MemoryProvider = ({ children }: MemoryProviderType) => {
   const [cards, setCards] = useState<IMemoryCard[]>(initialState.cards);
   const [turn, setTurn] = useState<number>(initialState.turn);
+  const [choiceOne, setChoiceOne] = useState<IMemoryCard | null>(null);
+  const [choiceTwo, setChoiceTwo] = useState<IMemoryCard | null>(null);
+  const [disabledCards, setDisabledCards] = useState<boolean>(false);
 
+  /**
+   * @description
+   * This function is used to start the game
+   * It shuffles the cards and sets the turn to 0
+   * @returns void
+   */
   const shuffleCards = () => {
     const shuffledCards = [...CardArray, ...CardArray]
       .sort(() => Math.random() - 0.5)
@@ -38,22 +49,91 @@ const MemoryProvider = ({ children }: MemoryProviderType) => {
     setCards(shuffledCards);
   };
 
+  /**
+   * @description
+   * This function is used to start the game
+   * It shuffles the cards and sets the turn to 0
+   * @returns void
+   */
   const startGame = () => {
     shuffleCards();
+    setTurn(0);
   };
 
+  /**
+   * @description
+   * This function is used to handle the click event on the card
+   * It flips the card and checks if the card is a match
+   * @param card
+   */
   const handleCardItemClick = (card: IMemoryCard) => {
-    setCards(prevCard =>
-      prevCard.map(c => {
-        if (c.id === card.id) {
-          card.isFlipped = true;
-          return card;
-        }
-        return c;
-      })
-    );
+    if (!disabledCards) {
+      setCards(prevCard =>
+        prevCard.map(c => {
+          if (c.id === card.id) {
+            card.isFlipped = true;
+            return card;
+          }
+          return c;
+        })
+      );
+    }
+
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
 
+  /**
+   * @description
+   * This function is used to reset the cards
+   * @returns void
+   */
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurn(prevTurn => prevTurn + 1);
+    setDisabledCards(false);
+  };
+
+  /**
+   * @description
+   * This function is used to check if the cards are a match
+   * @returns void
+   */
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabledCards(true);
+      if (choiceOne.image === choiceTwo.image) {
+        setCards(prevCards =>
+          prevCards.map(card => {
+            if (card.id === choiceOne?.id || card.id === choiceTwo?.id) {
+              card.isMatched = true;
+              card.isFlipped = true;
+            }
+            return card;
+          })
+        );
+        resetTurn();
+      } else {
+        setTimeout(() => {
+          setCards(prevCard => {
+            return prevCard.map(card => {
+              if (!card.isMatched) {
+                return { ...card, isFlipped: false };
+              }
+              return card;
+            });
+          });
+          resetTurn();
+        }, 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  /**
+   * @description
+   * This function is used to check if the cards are a match
+   * @returns void
+   */
   useEffect(() => {
     shuffleCards();
   }, []);
@@ -63,7 +143,8 @@ const MemoryProvider = ({ children }: MemoryProviderType) => {
     setCards,
     startGame,
     turn,
-    handleCardItemClick
+    handleCardItemClick,
+    disabledCards
   };
 
   return (
