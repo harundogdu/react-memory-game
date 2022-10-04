c4a0cd1f5d42687ff41bde0c2509bdad8a9607e8
Harun Doğdu <Harundogdu06@gmail.com>
2022-10-04 18:00:52 +0300

    feat(memory-game) : app layout made it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css';
 import { CardContainer } from './containers';
+import { useMemoryCards } from './contexts/memoryContext';
 
 function App() {
-
+  const { startGame, turn } = useMemoryCards();
   return (
     <div className='app'>
       <h1>Memory Game</h1>
-      <button>Start a New Game</button>
+      <button onClick={startGame}>Start a New Game</button>
       <CardContainer />
-      <p>Turn : 0</p>
+      <h2>Turn : {turn}</h2>
     </div>
   );
 }
