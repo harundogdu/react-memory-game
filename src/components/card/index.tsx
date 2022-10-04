d8ace6fcd1e09b66b9266e234ff31fe71a9ee0a5
Harun Doğdu <Harundogdu06@gmail.com>
2022-10-04 18:20:34 +0300

    feat(memory-game) : handle card item function used

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -3,12 +3,26 @@ import { IMemoryCard } from '../../types/memoryCard';
 
 type ICardProps = {
   card: IMemoryCard;
+  onClick: (card: IMemoryCard) => void;
 };
 
-const Card: React.FC<ICardProps> = ({ card }) => {
+const Card: React.FC<ICardProps> = ({ card, onClick }) => {
+
+  const handleClick = () => {
+    onClick(card);
+  };
+
   return (
     <div className='card'>
-      <img src={card.image} alt={card.name} />
+      <div className={`${card.isFlipped ? 'flipped' : ''} `}>
+        <img className='back' src={card.image} alt={card.name} />
+        <img
+          className='front'
+          src='/img/cover.png'
+          alt='Card Cover'
+          onClick={handleClick}
+        />
+      </div>
     </div>
   );
 };
--- src/containers/cardContainer/index.tsx
+++ src/containers/cardContainer/index.tsx
@@ -3,12 +3,11 @@ import { Card } from '../../components';
 import { useMemoryCards } from '../../contexts/memoryContext';
 
 const CardContainer = () => {
-  const { cards, setCards } = useMemoryCards();
-  console.log(cards)
+  const { cards, handleCardItemClick } = useMemoryCards();
   return (
     <div className='card-container'>
       {cards.map((card, index) => (
-        <Card card={card} key={card?.id || index} />
+        <Card card={card} key={card?.id || index} onClick={handleCardItemClick} />
       ))}
     </div>
   );
