c5ab74a81690e0eefa31dfb60b5de0b92473c078
Harun Doğdu <Harundogdu06@gmail.com>
2022-10-04 17:33:03 +0300

    feat(memory-game) : layout started

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import './App.css'
+import './App.css';
+import { CardContainer } from './containers';
 
 function App() {
 
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
+    <div className='app'>
+      <h1>Memory Game</h1>
+      <button>Start a New Game</button>
+      <CardContainer />
+      <p>Turn : 0</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
